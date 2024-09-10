5f612fbffe865dc7f0d375ac740f96f9f01e3b6c
oliverkeen15 <oliverkeen15@gmail.com>
2024-09-10 17:33:36 +0100

    Delete user fixed

--- Dev/deleteUser.js
+++ Dev/deleteUser.js
@@ -2,8 +2,8 @@ const { Users } = require('../Database/Objects');
 const id = "268097383982825474";
 
 (async() => {
-    const user = await Users.findOne({ where: { user_id: id } });
-    await user.Destroy();
+    const user = await Users.destroy({ where: { user_id: id } });
+    await user.destroy();
     console.log("Done!");
 })();
 
