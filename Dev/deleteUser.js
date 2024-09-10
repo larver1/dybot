2ba193f560e24580c04d881db440e584654f31f2
oliverkeen15 <oliverkeen15@gmail.com>
2024-09-10 17:34:42 +0100

    Fix script final

--- Dev/deleteUser.js
+++ Dev/deleteUser.js
@@ -2,8 +2,7 @@ const { Users } = require('../Database/Objects');
 const id = "268097383982825474";
 
 (async() => {
-    const user = await Users.destroy({ where: { user_id: id } });
-    await user.destroy();
+    await Users.destroy({ where: { user_id: id } });
     console.log("Done!");
 })();
 
