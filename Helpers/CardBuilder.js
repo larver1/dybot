6cf62599cffbc3a8064b90004ffff14b408009f4
oliverkeen15 <oliverkeen15@gmail.com>
2026-04-28 20:41:26 +0100

    Add pullrates and updated open view

--- Helpers/CardBuilder.js
+++ Helpers/CardBuilder.js
@@ -5,31 +5,126 @@ const cardTypes = JSON.parse(fs.readFileSync('./Objects/CardData.json'));
  * A class to help with messages and reduce duplicate code
  */
 module.exports = class CardBuilder {
+    static rarityOdds = {
+        "1": {
+            "Common": 0.75,
+            "Uncommon": 0.25
+        },
+        "2": {
+            "Common": 0.75,
+            "Uncommon": 0.25
+        },
+        "3": {
+            "Common": 0.75,
+            "Uncommon": 0.25
+        },
+        "4": {
+            "Common": 0.22,
+            "Uncommon": 0.42,
+            "Rare": 0.3,
+            "Legendary": 0.05,
+            "Mythical": 0.01
+        },
+        "5": {
+            "Uncommon": 0.25,
+            "Rare": 0.53,
+            "Legendary": 0.15,
+            "Mythical": 0.07  
+        }
+    }
+
+    static cardTypeOdds = {
+        "Common": {
+            "gold": 0.03,
+            "star": 0.12,
+            "none": 0.85
+        },
+        "Uncommon": {
+            "gold": 0.05,
+            "star": 0.15,
+            "none": 0.8
+        },
+        "Rare": {
+            "gold": 0.06,
+            "star": 0.19,
+            "none": 0.75
+        },
+        "Legendary": {
+            "gold": 0.08,
+            "star": 0.21,
+            "none": 0.71
+        },
+        "Mythical": {
+            "gold": 0.1,
+            "star": 0.25,
+            "none": 0.65
+        }
+    };
+
+    static holoOdds = {
+        "Common": {
+            "gold": 0.35,
+            "none": 0.18
+        },
+        "Uncommon": {
+            "gold": 0.35,
+            "none": 0.23
+        },
+        "Rare": {
+            "gold": 0.35,
+            "none": 0.28
+        },
+        "Legendary": {
+            "gold": 0.35,
+            "none": 0.32
+        },
+        "Mythical": {
+            "gold": 0.35,
+            "none": 0.35
+        }
+    };
+
+    /**
+     * @param {UserCards} card 
+     */
+    static calculatePullOdds(card) {
+        let totalRarityOdds = 0;
+        let totalCardTypeOdds = 0;
+        let totalHoloOdds = 0;
+        for (const [slot, rarities] of Object.entries(this.rarityOdds)) {
+            for (const [rarity, odds] of Object.entries(rarities)) {
+                if(rarity === card.rarity) {
+                    totalRarityOdds += odds;
+
+                    if (card.gold) { totalCardTypeOdds += this.cardTypeOdds[rarity].gold; }
+                    else if (card.star) { totalCardTypeOdds += this.cardTypeOdds[rarity].star; }
+                    else { totalCardTypeOdds += this.cardTypeOdds[rarity].none; }
+
+                    if(card.gold){ totalHoloOdds += card.holo ? 1 - this.holoOdds[rarity].gold : this.holoOdds[rarity].gold }
+                    else { totalHoloOdds += card.holo ? 1 - this.holoOdds[rarity].none : this.holoOdds[rarity].none; }
+                }
+            }
+        }
+        
+        const numSlots = Object.keys(this.rarityOdds).length;
+        let averageRarityOdds = totalRarityOdds / numSlots;
+        let averageCardTypeOdds = totalCardTypeOdds / numSlots;
+        let averageHoloOdds = totalHoloOdds / numSlots;
+
+        return averageRarityOdds * averageCardTypeOdds * averageHoloOdds;
+    }
+    
     /**
      * Roll a card rarity
      * @param {number} slot
      */
     static rollRarity(slot) {
-        let rand = Math.random();
-        switch(slot) {
-            case 1:
-            case 2:
-            case 3:
-                if(rand > 0.25) return "Common";
-                else return "Uncommon";
-            case 4:
-                if(rand > 0.78) return "Common";
-                else if(rand > 0.36) return "Uncommon";
-                else if(rand > 0.06) return "Rare";
-                else if(rand > 0.01) return "Legendary";
-                else return "Mythical";
-            case 5:
-                if(rand > 0.75) return "Uncommon";
-                else if(rand > 0.22) return "Rare";
-                else if(rand > 0.07) return "Legendary";
-                else return "Mythical";
-            default:
-                throw new Error('Invalid slot rarity!');
+        const rand = Math.random();
+        const rarities = this.rarityOdds[slot];
+        let currentOdds = 0.0;
+        for (const [rarity, odds] of Object.entries(rarities)) {
+            currentOdds += odds;
+            if (rand < currentOdds) { return rarity; }
         }
     }
 
@@ -41,35 +136,11 @@ module.exports = class CardBuilder {
         const resultObj = { gold: false, star: false, holo: false };
         const goldStarRng = Math.random();
         const holoRng = Math.random();
-        switch(rarity) {
-            case "Common":
-                if(goldStarRng < 0.03) resultObj.gold = true;
-                else if(goldStarRng < 0.15) resultObj.star = true;
-                if(holoRng < ( resultObj.gold ? 0.35 : 0.18 ) ) resultObj.holo = true;
-                break;
-            case "Uncommon":
-                if(goldStarRng < 0.05) resultObj.gold = true;
-                else if(goldStarRng < 0.2) resultObj.star = true;
-                if(holoRng < ( resultObj.gold ? 0.35 : 0.23 ) ) resultObj.holo = true;
-                break;
-            case "Rare":
-                if(goldStarRng < 0.06) resultObj.gold = true;
-                else if(goldStarRng < 0.25) resultObj.star = true;
-                if(holoRng < ( resultObj.gold ? 0.35 : 0.28 ) ) resultObj.holo = true;
-                break;
-            case "Legendary":
-                if(goldStarRng < 0.08) resultObj.gold = true;
-                else if(goldStarRng < 0.29) resultObj.star = true;
-                if(holoRng < ( resultObj.gold ? 0.35 : 0.32 ) ) resultObj.holo = true;
-                break;
-            case "Mythical":
-                if(goldStarRng < 0.1) resultObj.gold = true;
-                else if(goldStarRng < 0.35) resultObj.star = true;
-                if(holoRng < ( resultObj.gold ? 0.35 : 0.35 ) ) resultObj.holo = true;
-                break;
-            default:
-                throw new Error('Invalid slot rarity!');
-        }
+        
+        if (goldStarRng < this.cardTypeOdds[rarity].gold) { resultObj.gold = true; }
+        else if (goldStarRng < this.cardTypeOdds[rarity].gold + this.cardTypeOdds[rarity].star) { resultObj.star = true; }
+        if (holoRng < this.holoOdds[rarity][`${resultObj.gold ? 'gold' : 'none'}`]) { resultObj.holo = true; }
+
         return resultObj;
     }
 
